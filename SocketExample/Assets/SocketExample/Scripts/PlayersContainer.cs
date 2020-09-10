2a962a92981c0d8cfeadcdfa0141cd9101f84186
Kateryna Levshova <>
2020-09-10 16:07:40 -0700

    Added initialization of playersList in the PlayersContainer

--- SocketExample/Assets/SocketExample/Scripts/PlayersContainer.cs
+++ SocketExample/Assets/SocketExample/Scripts/PlayersContainer.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayersContainer : MonoBehaviour
 {
+    public List<Player> playersList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitPlayersList();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InitPlayersList()
+    {
+        Debug.Log("[PlayersContainer]-> CreatePlayersList");
+        playersList = new List<Player>(GetComponentsInChildren<Player>(true));
     }
 }
