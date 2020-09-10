c83ddb39da6e6c3587b0a4783ada058db40bbad3
Kateryna Levshova <>
2020-09-10 14:46:23 -0700

    Added core classes for the example

---
+++ SocketExample/Assets/SocketExample/Scripts/Command.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+    public string name;
+    public List<string> parameters;
+
+    virtual public void Execute(List<string> parameters)
+    {
+    }
+}
---
+++ SocketExample/Assets/SocketExample/Scripts/ProtocolList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Lists of all commands used to communicate between Client and Server side.
+ * All names must correspond to the Client-Sever Protocol document.
+ * @author Kateryna Levshova
+ */
+public class ProtocolList 
+{
+    public const string PLAYER_UPDATE_AVATAR = "PLAYER_UPDATE_AVATAR";
+    //TODO: add more commands here
+}
