cac173193fb99f223d8bfbd7b986c217c8ce1c84
Kateryna Levshova <>
2020-09-29 11:22:18 -0700

    Added implementation for CmdUpdateAvatarResponse class

--- SocketExample/Assets/SocketExample/Scripts/CmdUpdateAvatarResponse.cs
+++ SocketExample/Assets/SocketExample/Scripts/CmdUpdateAvatarResponse.cs
@@ -1,7 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CmdUpdateAvatarResponse : Command
 {
+    public delegate void ActionUpdateAvatar(int playerId);
+    public static event ActionUpdateAvatar OnUpdateAvatar;
+
+    public override void Execute<T>(T value)
+    {
+        OnUpdateAvatar?.Invoke((int)paramsDict["PlayerId"]);
+    }
 }
