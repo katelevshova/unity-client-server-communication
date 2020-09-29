41bf3ff8d7f0d79c052ed58a68a73cd0e81723d3
Kateryna Levshova <>
2020-09-29 11:19:37 -0700

    Added ProcessResponse into DataParser

--- SocketExample/Assets/SocketExample/Scripts/DataParser.cs
+++ SocketExample/Assets/SocketExample/Scripts/DataParser.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class DataParser
 {
-   
+    public void ParseResponseMsg(string message)
+    {
+        // parse message here based on Header and footer 
+        // create the list of messages
+        // for all items in the  messages_list do ProcessResponse
+
+
+    }
+
+
+    public void ProcessResponse(Command cmd)
+    {
+        switch (cmd.name)
+        {
+            case ProtocolList.PLAYER_UPDATE_AVATAR:
+                {
+                    CmdUpdateAvatarResponse cmdUpdateAvatarResponse = new CmdUpdateAvatarResponse();
+                    cmdUpdateAvatarResponse.Execute(cmd.paramsDict);
+                    break;
+                }
+            //TODO: add more responses here
+        }
+    }
+
+
+
 }
